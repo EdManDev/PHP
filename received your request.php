e4edb1da1a8a0b3bde5a9aceeecbbdc2ec26a999
EdManDev <ed.manigat@gmail.com>
2019-03-06 14:13:29 -0400

    ,

--- received your request.php
+++ received your request.php
@@ -5,7 +5,7 @@
 		$phone=$_POST['phone'];
 		$msg=$_POST['msg'];
 
-		$to='contact.haitieco@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$to='ed.manigat@hotmail.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
