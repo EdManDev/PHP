69b06916f80ee0a93525bc217615996f163e956f
EdManDev <ed.manigat@gmail.com>
2019-06-18 08:22:59 -0400

    commit

---
+++ un chat/php/chat.php
@@ -0,0 +1,3 @@
+<?php
+session_start(); include('phpscripts/functions.php'); $db = db_connect();
+?>
---
+++ un chat/php/set-status.php
@@ -0,0 +1,6 @@
+<?php
+if(user_verified()) { $insert = $db->prepare('
+UPDATE chat_online SET online_status = :status WHERE online_user =:user ');
+$insert->execute(array( 'status' => $_POST['status'], 'user' => $_SESSION['id']
+)); }
+?>
