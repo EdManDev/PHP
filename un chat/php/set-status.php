deb4f5189dc4a0dfb4968eab844b84596b5720ee
EdManDev <ed.manigat@gmail.com>
2019-06-18 22:21:05 -0400

    n

--- un chat/php/set-status.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(user_verified()) { $insert = $db->prepare('
-UPDATE chat_online SET online_status = :status WHERE online_user =:user ');
-$insert->execute(array( 'status' => $_POST['status'], 'user' => $_SESSION['id']
-)); }
-?>
