9d715bfa4f157a50aec568bb2d4ce37d7386b391
EdManDev <ed.manigat@gmail.com>
2019-06-25 22:36:26 -0400

    update

--- un_chat/phpscripts/functions.php
+++ un_chat/phpscripts/functions.php
@@ -1,47 +1,31 @@
 <?php
-  function db_connect() {
-    
-  // définition des variables de connexion à la base de données try {
-    
-  $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION; 
-  
-  // INFORMATIONS DE CONNEXION
-  
-  $host = 'localhost';
-  $dbname = 'un_chat';
-  $user = 'root'; 
-  $password = 'root';
-  
-   // FIN DES DONNEES
-  
-  $db = new PDO('mysql:host='.$host.';dbname='.$dbname.'', $user, $password, $pdo_options);
-    return $db;
-    
-  } 
-  
-  catch (Exception $e) {
-    die('Erreur de connexion : ' . $e->getMessage()); 
+class db_connect{
+  private $dbHost ='localhost';
+  private $dbUser = 'root';
+  private $dbPass = 'root';
+  private $dbName = 'un_chat';
+  //conección 
+  public function conectDB(){
+    $mysqlConnect = "mysql:host=$this->dbHost;dbname=$this->dbName";
+    $dbConnecion = new PDO($mysqlConnect, $this->dbUser, $this->dbPass);
+    $dbConnecion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConnecion;
   }
-
-  
-
+}
 
 
-  // function user_verified() {
-  //   return isset($_SESSION['id']); 
-  // }
+function user_verified() {
+  return isset($_SESSION['id']); 
+}
 
-
-
-//   function urllink($content='') {
-//     $content = preg_replace('#(((https?://)|(w{3}\.))+[a-zA-Z0-9&;\#\.\?=_/-]+\.([a-z]{2,4})([a-zA-Z0-9&;\#\.\?=_/-]+))#i', '<a href="$0" target="_blank">$0</a>', $content);
-//     // Si on capte un lien tel que www.test.com, il faut rajouter le http://
-    
-//     if(preg_match('#<a href="www\.(.+)" target="_blank">(.+)<\/a>#i', $content)) {
-//     $content = preg_replace('#<a href="www\.(.+)" target="_blank">(.+)<\/a>#i', '<a href="http://www.$1" target="_blank">www.$1</a>', $content);
-//     //preg_replace('#<a href="www\.(.+)">#i', '<a href="http://$0">$0</a>', $content);
-//     }
-//     $content = stripslashes($content);
-//     return $content; 
-//   }
-// ?>
+function urllink($content='') {
+    $content = preg_replace('#(((https?://)|(w{3}\.))+[a-zA-Z0-
+    9&;\#\.\?=_/-]+\.([a-z]{2,4})([a-zA-Z0-9&;\#\.\?=_/-]+))#i', '<a href="$0" target="_blank">$0</a>', $content);
+    // Si on capte un lien tel que www.test.com, il faut rajouter le http://
+    if(preg_match('#<a href="www\.(.+)" target="_blank">(.+)<\/a>#i', $content)) {
+    $content = preg_replace('#<a href="www\.(.+)" target="_blank">(.+)<\/a>#i', '<a href="http://www.$1" target="_blank">www.$1</a>', $content);
+    //preg_replace('#<a href="www\.(.+)">#i', '<a href="http://$0">$0</a>', $content);
+  }
+    $content = stripslashes($content);
+    return $content; 
+}
