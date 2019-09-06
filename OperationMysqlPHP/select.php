167984cc5b1934599e6a4b258091133fc426c7d8
EdMan <mojave@EdMans-MBP.local>
2019-09-06 10:28:47 -0400

    update

--- OperationMysqlPHP/select.php
+++ OperationMysqlPHP/select.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./select.css">
   <title>Select & Show</title>
 </head>
 <body>
@@ -19,16 +20,61 @@
         // output data of each row
         while($row = $result->fetch_assoc()) {
           echo 
-          "<br/>"."THE ID:".$row["id"].
-          "<br/>"."THE ENGINE IS:".$row["engine"].
-          "<br/>"."THE TRANSMITION IS:".$row["transmission"].
-          "<br/>"."NBR THE DOOR:".$row["door_no"].
-          "<br/>"."AGE:".$row["age"].
+          "<br/>"."<b>ID: </b>".$row["id"].
+          "<br/>"."<b>THE ENGINE IS: </b>".$row["engine"].
+          "<br/>"."<b>THE TRANSMITION IS: </b>".$row["transmission"].
+          "<br/>"."<b>NBR THE DOOR: </b>".$row["door_no"].
+          "<br/>"."<b>AGE: </b>".$row["age"].
           "<br>"."<br>";
+
+          // Table Above
+
+          $html="
+            <html>
+              <head></head>
+              <title>some work</title>
+              <body>
+                <table>
+                  <tr>
+                    <th><b>ID:</b></th>
+                    <th><b>ENGINE IS:</b></th>
+                    <th><b>TRANSMITION:</b></th>
+                    <th><b>NBR THE DOOR:</b></th>
+                    <th><b>AGE:</b></th>
+                  </tr>
+                  <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                    <td>2X</td>
+                    <td>9</td>
+                  </tr>
+                  <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                    <td>2X</td>
+                    <td>9</td>
+                  </tr>
+                  <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                    <td>2X</td>
+                    <td>9</td>
+                  </tr>
+                </table>
+              </body>
+            </html>";
+// foter
+          $html.="
+          <div><h1>footer</h1></div>
+          ";
       }
     } else {
         echo "0 results";
     }
+    echo $html;
     $conn->close();
     ?>
 
